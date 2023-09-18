7b9dccff54dfcd9487734c9f7819c630a290cee8
Colin Huber <cbhuber@gmail.com>
2023-09-18 17:26:45 -0600

    feat: about static page.

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+
+// Static page, not updated based on API fetching; essentially hardcoded page
+export const dynamic = "force-static";
+
+export const metadata: Metadata = {
+  title: "About Us",
+  description: "About NextSpace",
+};
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>We are a social media company that wants to bring people together!</p>
+    </div>
+  );
+}
