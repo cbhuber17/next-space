033e49aec039e2cf892155427b44b82eb26be37e
Colin Huber <cbhuber@gmail.com>
2023-09-18 20:03:51 -0600

    feat: auth via next-auth; google and facebook providers.

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import CredentialsProvider from "next-auth/providers/credentials";
+// import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// import { prisma } from '@/lib/prisma';
+
+export const authOptions: NextAuthOptions = {
+  // session: {
+  //   strategy: 'jwt',
+  // },
+  secret: process.env.NEXTAUTH_SECRET,
+  //   adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    }),
+    //   CredentialsProvider({
+    //     name: 'as Guest',
+    //     credentials: {},
+    //     async authorize(credentials) {
+    //       const user = {
+    //         id: Math.random().toString(),
+    //         name: 'Guest',
+    //         email: 'guest@example.com',
+    //       };
+    //       return user;
+    //     },
+    //   }),
+    // ],
+    // callbacks: {
+    //   async signIn({ user }) {
+    //     // block signin if necessary
+    //     return true;
+    //   }
+    // },
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
