db59b36988cca8ecc08fcd9b58ba8f7fbad9df53
Colin Huber <cbhuber@gmail.com>
2023-09-19 18:53:21 -0600

    fix: remove fb provider, add guest.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
+// import FacebookProvider from "next-auth/providers/facebook";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
@@ -17,30 +17,29 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_CLIENT_ID!,
+    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    // }),
+    CredentialsProvider({
+      name: "as a Guest",
+      credentials: {},
+      async authorize(credentials) {
+        const user = {
+          id: Math.random().toString(),
+          name: "Guest",
+          email: "guest@example.com",
+        };
+        return user;
+      },
     }),
-    //   CredentialsProvider({
-    //     name: 'as Guest',
-    //     credentials: {},
-    //     async authorize(credentials) {
-    //       const user = {
-    //         id: Math.random().toString(),
-    //         name: 'Guest',
-    //         email: 'guest@example.com',
-    //       };
-    //       return user;
-    //     },
-    //   }),
-    // ],
-    // callbacks: {
-    //   async signIn({ user }) {
-    //     // block signin if necessary
-    //     return true;
-    //   }
-    // },
   ],
+  callbacks: {
+    async signIn({ user }) {
+      // block signin if necessary
+      return true;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
