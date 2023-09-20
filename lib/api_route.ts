9f998ef084756c828104c2e218b937495b24b32c
Colin Huber <cbhuber@gmail.com>
2023-09-19 19:23:16 -0600

    env: deployment api route.

--- lib/api_route.ts
+++ lib/api_route.ts
@@ -1,6 +1,6 @@
 let host: string;
 
 if (process.env.NEXT_ENV === "dev") host = "http://localhost:3000";
-else host = "";
+else host = "https://next-space-lf4c5iby3-cbhuber17.vercel.app";
 
 export default host;
